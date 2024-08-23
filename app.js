083cbd2d180e5c8f69ecf91aa1769b7ce20478ab
Hachounet <hachounet.github@gmx.fr>
2024-08-23 11:30:53 +0200

    FEAT Cors / HTTPS

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
 const express = require("express");
-const { PrismaClient } = require("@prisma/client");
-const passport = require("passport");
+// const passport = require("passport");
 const path = require("path");
+const helmet = require("helmet");
+const cors = require("cors");
+const fs = require("fs");
+const https = require("https");
 
 const indexRouter = require("./routes/indexRouter");
 const postsRouter = require("./routes/postsRouter");
-const dashboardRouter = require("./routes/dashboard");
+const dashboardRouter = require("./routes/dashboardRouter");
 
 const app = express();
+
+const options = {
+  key: fs.readFileSync(process.env.SSL_KEY_PATH || "key.pem"),
+  cert: fs.readFileSync(process.env.SSL_CERT_PATH || "cert.pem"),
+};
+
+const corsOptions = {
+  origin: process.env.ALLOWED_ORIGINS || "*",
+  methods: "GET, HEAD, PUT, PATCH, POST,DELETE",
+  credentials: true,
+  allowedHeaders: ["Authorization", "Content-Type"],
+};
+
+app.use(cors(corsOptions));
+app.use(helmet());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-require("./db/passport");
-app.use(passport.initialize());
-
-app.use((req, res, next) => {
-  // Middleware to declare locals
-
-  next();
-});
+// require("./db/passport");
+// app.use(passport.initialize());
 
 app.use("/", indexRouter);
-app.use("/posts", postsRouter);
-app.use("/dashboard", dashboardRouter);
+// app.use("/posts", postsRouter);
+// app.use("/dashboard", dashboardRouter);
 
 // Err handling
 app.use((err, req, res, next) => {
@@ -49,6 +61,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server launched on PORT ${PORT}`);
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
